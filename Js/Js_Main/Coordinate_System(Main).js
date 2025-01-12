ce1a13a63742eeb7172cba9b83d928ab303fa468
MoatezGr <garrachmoatez50@gmail.com>
2025-01-12 18:21:44 +0100

    1/12/2025 (up3)

--- Js/Js_Main/Coordinate_System(Main).js
+++ Js/Js_Main/Coordinate_System(Main).js
@@ -6,15 +6,12 @@
 
 
                                 //-------------VAR-------------//
-        let Button_clicked = false
         let i = 0
 
 
                                 //-------------FUNCTIONS-------------//    
         function Recover_WebPage_Colors() {
             document.body.style.background = localStorage.getItem("Second_Color")
-            Square_1.style.background = localStorage.getItem("First_Color")
-            Square_2.style.background = localStorage.getItem("First_Color")
             Start_Button.style.color = localStorage.getItem("Text_Color")
             Start_Button.style.borderColor = localStorage.getItem("First_Color")
         }
@@ -49,40 +46,13 @@
                                 //-------------CODE-------------//
         if (localStorage.getItem('First_Color') == null) {
             location.assign("./Coordinate_System(Costomize).html")
-        }   
+        } 
         Recover_WebPage_Colors()                
         Meteor_Animation()
-        
-
-        Start_Button.onmouseenter = function () {
-            if (Button_clicked === false) {
-                console.log("Focus On");
-                Square_1.style.animationName = "Square_1_MouseEnter"
-                Square_2.style.animationName = "Square_2_MouseEnter"       
-            }
-        }
-
-        Start_Button.onmouseout = function () {
-            if (Button_clicked === false) {
-                console.log("Focus Out");
-                Square_1.style.animationName = "Square_1_MouseOut"
-                Square_2.style.animationName = "Square_2_MouseOut"
-            }
-        }
 
 
 
         Start_Button.onclick = function () {
             console.log("Clicked");
-            Button_clicked = true
-            console.log(Button_clicked);
-            Square_1.style.animationDuration = "2s"
-            Square_2.style.animationDuration = "2s"
-            Square_1.style.animationName = "Square_1_Button_Clicked"
-            Square_2.style.animationName = "Square_2_Button_Clicked"
-            Start_Button.style.animationName = "Start_Button_Animation"
-            setTimeout(() => {
-                location.assign("./Coordinate_System.html")
-            }, 1700);
-
+            location.assign("./Coordinate_System.html")
         }

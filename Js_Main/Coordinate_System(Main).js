b3a3e9e40f3e6aa68b3d4db32aa9764c776d54c5
MoatezGr <garrachmoatez50@gmail.com>
2025-02-14 19:34:00 +0100

    2/14/2025(10up)

--- Js_Main/Coordinate_System(Main).js
+++ Js_Main/Coordinate_System(Main).js
@@ -1,6 +1,6 @@
 
                                 //-------------IMPORT-------------//
-import * as THREE from "./node_modules/three/build/three.js";
+import * as THREE from "/node_modules/three/build/three.js";
 import { GLTFLoader } from "/node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 
                                 //-------------Html Elements-------------//
